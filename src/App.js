25080e3904f809e5e608792e01f8bd1a221327c5
ZalakJoshi1101 <zalakjoshi1101@gmail.com>
2024-01-09 19:38:50 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <h1>this is the demo</h1>
         <h1>thi sis the demo 2</h1>
         <h1>Hello bro..</h1>
+        <h1>hello sister..!</h1>
 
         {/* thayu change  yes bro its done... */}
         <img src={logo} className="App-logo" alt="logo" />
